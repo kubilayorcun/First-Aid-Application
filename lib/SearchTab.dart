4abba7d9e2ca9e98d8d4588e0a5803133320f3c4
mertsariozkan <mert.sariozkan@hotmail.com>
2019-05-05 20:50:19 +0300

    Search in subcategories feature is added.

--- lib/SearchTab.dart
+++ lib/SearchTab.dart
@@ -1,58 +1,92 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'CustomWidgets/SubMethodListItem.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'CustomWidgets/ListItem.dart';
+import 'MethodPage.dart';
+
 class SearchTab extends StatefulWidget {
   List<SubMethodListItem> favList;
+
   SearchTab(List<SubMethodListItem> favList) {
-   this.favList = favList;
+    this.favList = favList;
   }
-  String searchText = "";
+
   Text text;
-  //final items = List<String>.generate(10000, (i) => "Item $i");
-  List<String> items = new List<String>();
 
   @override
   SearchTabState createState() => new SearchTabState(favList);
-
 }
 
 class SearchTabState extends State<SearchTab> {
   TextEditingController editingController = new TextEditingController();
   List<SubMethodListItem> favList;
+
   SearchTabState(List<SubMethodListItem> favList) {
-    this.favList=favList;
+    this.favList = favList;
   }
 
-  final defaultList = List<String>();
-  var items = List<String>();
+  List<String> defaultList = [];
+  List<String> defaultBaseList = [];
+  List<String> items = List<String>();
+  List<String> baseItems = List<String>();
+
+  Future<void> getAllSubCategories() async {
+    List<String> categoryNames = [];
+
+    await for (var snapshot
+        in Firestore.instance.collection("CategoryNames").snapshots()) {
+      for (var a in snapshot.documents) {
+        categoryNames.add(a.data["Name"]);
+        print(a.data["Name"]);
+      }
+      break;
+    }
+
+    print("mert");
+    for (var a in categoryNames) {
+      await for (var snapshot in Firestore.instance.collection(a).snapshots()) {
+        for (var b in snapshot.documents) {
+          setState(() {
+            defaultList.add(b.data["Name"]);
+            defaultBaseList.add(a);
+            print(b.data["Name"]);
+          });
+        }
+        break;
+      }
+    }
+    setState(() {
+      items.addAll(defaultList);
+      baseItems.addAll(defaultBaseList);
+    });
+  }
 
   @override
   void initState() {
-    items.addAll(defaultList);
+    getAllSubCategories();
     super.initState();
   }
 
-
-  void filterSearchResults(String query){
-    List<String> dummySearchList = List<String>();
-    dummySearchList.addAll(defaultList);
-    if(query.isNotEmpty){
-      List<String> dummyListData = List<String>();
-      dummySearchList.forEach((item){
-        if(item.contains(query)){
-          dummyListData.add(item);
+  void filterSearchResults(String query) {
+    setState(() {
+      items.clear();
+      baseItems.clear();
+    });
+    if (query.isNotEmpty) {
+      for (int i = 0; i < defaultList.length; i++) {
+        if (defaultList[i].contains(query)) {
+          setState(() {
+            items.add(defaultList[i]);
+            baseItems.add(defaultBaseList[i]);
+          });
         }
-      });
-      setState(() {
-        items.clear();
-        items.addAll(dummyListData);
-      });
-      return;
+      }
     } else {
+      print("here");
       setState(() {
-        items.clear();
         items.addAll(defaultList);
+        baseItems.addAll(defaultBaseList);
       });
     }
   }
@@ -72,61 +106,46 @@ class SearchTabState extends State<SearchTab> {
               child: TextField(
                 cursorColor: Colors.red,
                 cursorWidth: 2.0,
-                keyboardType: TextInputType.multiline ,
+                keyboardType: TextInputType.multiline,
                 maxLines: null,
-                style: TextStyle(
-                    fontSize: 17.0,
-                    color: Colors.black
-                ),
+                style: TextStyle(fontSize: 17.0, color: Colors.black),
                 onChanged: (value) {
                   filterSearchResults(value);
                 },
                 controller: editingController,
                 autofocus: true,
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(top: 8.0,bottom: 8.0, left: 8.0),
+                    contentPadding:
+                        EdgeInsets.only(top: 8.0, bottom: 8.0, left: 8.0),
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(22.0))
-                    ),
+                        borderRadius: BorderRadius.all(Radius.circular(22.0))),
                     hintText: "Search",
                     labelText: "Search",
                     hintStyle: TextStyle(),
-                    prefixIcon: Icon(Icons.search)
-                ),
+                    prefixIcon: Icon(Icons.search)),
               ),
             ),
             Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: Firestore.instance.collection('CategoryNames').snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) return LinearProgressIndicator();
-                    print(snapshot.data.documents[2].data.values.toList());
-                    return ListView.separated(
-                      padding: EdgeInsets.all(8.0),
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        defaultList.add(snapshot.data.documents[index].data['Name']);
-                        items.add(snapshot.data.documents[index].data['Name']);
-                        return ListItem(items[index],favList);
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return Divider(color: Colors.black);
-                      },
-                    );
-                  },
-                )
-            )
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    child: ListTile(title: Text(items[index])),
+                    onTap: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MethodPage(
+                                      items[index], baseItems[index])))
+                        },
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
-
-
 }
-
-
-
-
-
